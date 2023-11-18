543639c8e0b0122fc829c52996cb029fcf5c3173
parth thakor <parththakor2610@gmail.com>
2023-11-18 13:13:31 +0530

    first api called using the fetch with the starwar movies

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+const [movies , setMovies] = useState([]);
+async function getMovies (){
+ try{
+
+   const response = await fetch('https://swapi.dev/api/films/');
+   const data = await response.json();
+   const transformArray = data.results.map(movie =>{
+    return {
+      id : movie.episode_id ,  
+      title :movie.title ,
+      releaseDate :movie.release_date ,
+      openingText : movie.opening_crawl,
+
+    }
+  })
+  setMovies(transformArray);
+  }catch(err){
+    console.log(err);
+  }
+
+
+}
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={getMovies}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
