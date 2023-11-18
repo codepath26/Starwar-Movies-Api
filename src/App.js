190f3cc68a99d4945134dd59c79b5496ab6876f3
parth thakor <parththakor2610@gmail.com>
2023-11-18 14:41:23 +0530

    handelling the error for the fetch api using the try catch

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-const [movies , setMovies] = useState([]);
-const [isLoading , setIsLoading] = useState(false);
-async function getMovies (){
- try{
-    setIsLoading(true);
-   const response = await fetch(process.env.REACT_APP_URL);
-   const data = await response.json();
-   const transformArray = data.results.map(movie =>{
-    return {
-      id : movie.episode_id ,  
-      title :movie.title ,
-      releaseDate :movie.release_date ,
-      openingText : movie.opening_crawl,
-
+  const [movies, setMovies] = useState([]);
+  const [error , setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  async function getMovies() {
+    try {
+      setIsLoading(true);
+      setError(null);
+      const response = await fetch(process.env.REACT_APP_URL);
+      console.log(response.ok)
+      if(!response.ok){
+        throw new Error('Something Went Wrong')
+      }
+      const data = await response.json();
+      const transformArray = data.results.map((movie) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          releaseDate: movie.release_date,
+          openingText: movie.opening_crawl,
+        };
+      });
+      setMovies(transformArray);
+    } catch (err) {
+      setError(err.message);
+      
     }
-  })
-  setMovies(transformArray);
-  setIsLoading(false);
-  }catch(err){
-    console.log(err);
+    setIsLoading(false);
   }
-
-
-}
 
   return (
     <React.Fragment>
@@ -35,10 +39,11 @@ async function getMovies (){
         <button onClick={getMovies}>Fetch Movies</button>
       </section>
       <section>
-       {
-
-       isLoading ?  <h1>Loading...</h1>: <MoviesList movies={movies} />
-       }
+        {/* {isLoading ? <h1>Loading...</h1> : <MoviesList movies={movies} />} */}
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>Fount no Movie</p>}
+        {!isLoading &&  error && <p>{error}</p>}
+        {isLoading  && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
